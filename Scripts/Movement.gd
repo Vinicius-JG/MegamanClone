5e381507cfcacf6133a7452b187dbb31c9e754d8
Vinícius Gonçalves <viniciusdejgoncalves@gmail.com>
2023-09-30 14:55:35 -0300

    added shooting animations and slide mechanic

--- Scripts/Movement.gd
+++ Scripts/Movement.gd
@@ -9,42 +9,53 @@ var on_floor = true
 @onready var anim = $AnimationPlayer
 @onready var sprite = $Sprite
 
+var sliding = false
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and !Input.is_action_pressed("slide"):
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		if !sliding: velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if !sliding: velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 	handle_animations()
 	
+	if Input.is_action_pressed("slide") and Input.is_action_just_pressed("jump") and is_on_floor() and !sliding:
+		sliding = true
+		anim.play("slide")
+		velocity.x = -1 * SPEED * 1.5 if sprite.flip_h else 1 * SPEED * 1.5
+		print(velocity)
+		await get_tree().create_timer(0.25).timeout
+		velocity.x = 0
+		sliding = false
+	
 func handle_animations():
-	if velocity.x != 0 and is_on_floor():
-		anim.play("walk")
+	if !sliding:
+		if velocity.x != 0 and is_on_floor() and anim.current_animation != "shoot_walk":
+			anim.play("walk")
 
-	if velocity.x == 0 and is_on_floor() and anim.current_animation != "shoot":
-		anim.play("idle")
+		if velocity.x == 0 and is_on_floor() and anim.current_animation != "shoot":
+			anim.play("idle")
 
-	if velocity.y < 0 and !is_on_floor():
-		anim.play("jump")
-		on_floor = false
+		if velocity.y < 0 and !is_on_floor() and anim.current_animation != "shoot_jump":
+			anim.play("jump")
+			on_floor = false
 
-	if velocity.y > 0 and !is_on_floor():
-		anim.play("fall")
+		if velocity.y > 0 and !is_on_floor() and anim.current_animation != "shoot_jump":
+			anim.play("fall")
 
-	if velocity.x > 0:
-		sprite.set_flip_h(false)
+		if velocity.x > 0:
+			sprite.set_flip_h(false)
 
-	if velocity.x < 0:
-		sprite.set_flip_h(true)
+		if velocity.x < 0:
+			sprite.set_flip_h(true)
+
--- Scripts/Shooting.gd
+++ Scripts/Shooting.gd
@@ -2,6 +2,7 @@ extends Node
 
 @onready var anim = $"../AnimationPlayer"
 @onready var sprite = $"../Sprite"
+@onready var movement = $"../../Player"
 @onready var shot = load("res://Scenes/shot.tscn")
 
 @export var offset: float
@@ -15,9 +16,19 @@ func _process(delta):
 		self.position = Vector2(offset, self.position.y)
 		
 	
-	if Input.is_action_just_pressed("shoot"):
-		anim.play("shoot")
+	if Input.is_action_just_pressed("shoot") and !movement.sliding:
+		handle_animations()
 		var newShot = shot.instantiate()
 		get_node("/root").add_child(newShot)
 		newShot.init(self.global_position, -1 if sprite.flip_h else 1, shotSpeed)
 		newShot.position = self.global_position
+
+func handle_animations():
+	if anim.current_animation == "idle":
+		anim.play("shoot")
+		
+	if anim.current_animation == "walk":
+		anim.play("shoot_walk")
+		
+	if anim.current_animation == "jump" || anim.current_animation == "fall":
+		anim.play("shoot_jump")
